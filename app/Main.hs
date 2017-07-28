8896ce8fd5add637b948b3a666bae5c77d341f09
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-07-28 13:42:04 -0400

    completed exercise 1

--- app/Main.hs
+++ app/Main.hs
@@ -18,10 +18,24 @@ demoStrs cmd = do
   result <- runInterpreter $ setImports imports >> interpret cmd (as :: [String])
   demor cmd (show result)
 
+demoInts :: String -> IO ()
+demoInts cmd = do
+  result <- runInterpreter $ setImports imports >> interpret cmd (as :: [[Int]])
+  demor cmd (show result)
+
+demoBuls :: String -> IO ()
+demoBuls cmd = do
+  result <- runInterpreter $ setImports imports >> interpret cmd (as :: [[Bool]])
+  demor cmd (show result)
+
 main :: IO ()
 main = do
   putStrLn "CIS194 Homework 3"
 
   putStrLn "\nExercise 1"
-  demoStrs "skips \"ABCD\""
-  demoStrs "skips \"hello!\""
+  demoStrs "skips \"ABCD\"       "
+  demoStrs "skips \"hello!\"     "
+  demoInts "skips [1]          "
+  demoBuls "skips [True,False] "
+
+  putStrLn "\nExercise 2"
--- src/Golf.hs
+++ src/Golf.hs
@@ -4,15 +4,25 @@ module Golf (
 
 
 -- Exercise 1
+-- define a function "skips"
 -- the first list in the output should tbe the same as the input list
 -- the second list in the output should oontain every second element from the input list
 -- ..the nth list in the output should contain every nth element from the input list
 
-skipsi :: [a] -> Int -> [a]
-skipsi [] jump = []
-skipsi list jump = do
-  let sub = drop jump list
-  head sub : skipsi sub jump
+len :: [a] -> Int
+len [] = 0
+len (_:xs) = 1 + len xs
+
+skipsii :: [a] -> Int -> [a]
+skipsii list jump
+  | len list < jump  = []
+  | otherwise        = do
+      let sub = drop (jump-1) list
+      head sub : skipsii (drop 1 sub) jump
+
+skipsi :: [a] -> Int -> [[a]]
+skipsi list 1 = [list]
+skipsi list jump = skipsi list (jump-1) ++ [skipsii list jump]
 
 skips :: [a] -> [[a]]
-skips list = [skipsi list 1]
+skips list = skipsi list ((len list))
